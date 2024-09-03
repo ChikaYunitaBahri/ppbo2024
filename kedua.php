dcca37a0a6d4e4022a9d5c7cea81e2c86be4766a
ChikaYunitaBahri <h1101231020@student.untan.ac.id>
2024-09-03 16:42:37 +0700

    Praktikum 2C - Function Sederhana

---
+++ kedua.php
@@ -0,0 +1,34 @@
+<?php
+
+function kelilingLingkaran($jari) : float 
+{
+    return 2 * 3.14 * $jari;
+}
+
+function volumeBola($jari) : float
+{
+    return (4/3) * 3.14 * pow($jari, 3);
+}
+
+function volumeTabung($jari, $tinggi) : float 
+{
+    return 3.14 * $jari * $jari * $tinggi;
+}
+
+function volumeKerucut($jari, $tinggi) : float 
+{
+    return (1/3) * 3.14 * $jari * $jari * $tinggi;
+}
+
+
+$keliling = kelilingLingkaran($jari);
+echo "Keliling lingkaran adalah {$keliling}\n";
+
+$volume_bola = volumeBola($jari);
+echo "Volume bola adalah {$volume_bola}\n";
+
+$volume_tabung = volumeTabung($jari, $tinggi_tabung);
+echo "Volume tabung adalah {$volume_tabung}\n";
+
+$volume_kerucut = volumeKerucut($jari, $tinggi_kerucut);
+echo "Volume kerucut adalah {$volume_kerucut}\n";
