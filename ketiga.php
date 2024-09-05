7f1dc63f19b47c98beb85e4e40297301f4e87850
ChikaYunitaBahri <h1101231020@student.untan.ac.id>
2024-09-05 10:38:44 +0700

    Praktikum 4C - Membuat Constructor

--- ketiga.php
+++ ketiga.php
@@ -5,6 +5,10 @@ class Lingkaran
     const PHI = 3.14;
     public $jari_jari;
 
+    public function __construct($jari_jari) {
+        $this->jari_jari = $jari_jari;
+    }
+
     public function luas(): float {
         return self::PHI * $this->jari_jari * $this->jari_jari;
     }
@@ -19,6 +23,10 @@ class Bola
     const PHI = 3.14;
     public $jari_jari;
 
+    public function __construct($jari_jari) {
+        $this->jari_jari = $jari_jari;
+    }
+
     public function volume(): float {
         return (4 / 3) * self::PHI * pow($this->jari_jari, 3);
     }
@@ -28,6 +36,10 @@ class Tabung
 {
     const PHI = 3.14;
     public $jari_jari;
+
+    public function __construct($jari_jari) {
+        $this->jari_jari = $jari_jari;
+    }
 
     public function volume($tinggi): float {
         return self::PHI * pow($this->jari_jari, 2) * $tinggi;
@@ -39,14 +51,17 @@ class Kerucut
     const PHI = 3.14;
     public $jari_jari;
 
+    public function __construct($jari_jari) {
+        $this->jari_jari = $jari_jari;
+    }
+
     public function volume($tinggi): float {
         return (1 / 3) * self::PHI * pow($this->jari_jari, 2) * $tinggi;
     }
 }
 
 // Contoh penggunaan class Lingkaran
-$lingkaran = new Lingkaran();
-$lingkaran->jari_jari = 7;
+$lingkaran = new Lingkaran(7);
 $luas_lingkaran = $lingkaran->luas();
 $keliling_lingkaran = $lingkaran->keliling();
 
@@ -54,29 +69,25 @@ echo "Luas lingkaran adalah: " . $luas_lingkaran . " cm^2\n";
 echo "Keliling lingkaran adalah: " . $keliling_lingkaran . " cm\n";
 
 // Contoh penggunaan class Bola
-$bola = new Bola();
-$bola->jari_jari = 5;
+$bola = new Bola(5);
 $volume_bola = $bola->volume();
 
 echo "Volume bola adalah: " . $volume_bola . " cm^3\n";
 
 // Contoh penggunaan class Tabung
-$tabung = new Tabung();
-$tabung->jari_jari = 3;
+$tabung = new Tabung(3);
 $volume_tabung = $tabung->volume(10); 
 
 echo "Volume tabung adalah: " . $volume_tabung . " cm^3\n";
 
 // Contoh penggunaan class Kerucut
-$kerucut = new Kerucut();
-$kerucut->jari_jari = 4;
-$volume_kerucut = $kerucut->volume(10); 
+$kerucut = new Kerucut(4);
+$volume_kerucut = $kerucut->volume(10); // 
 
 echo "Volume kerucut adalah: " . $volume_kerucut . " cm^3\n";
 
 // Contoh penggunaan class Kerucut untuk objek nasi tumpeng
-$nasi_tumpeng = new Kerucut();
-$nasi_tumpeng->jari_jari = 4;
+$nasi_tumpeng = new Kerucut(4);
 $volume_nasi_tumpeng = $nasi_tumpeng->volume(10); 
 
 echo "Volume nasi tumpeng adalah: " . $volume_nasi_tumpeng . " cm^3\n";
