efe12e39ddc74aae461eb8d0300eab5e54c53c7c
ChikaYunitaBahri <h1101231020@student.untan.ac.id>
2024-10-18 13:53:34 +0700

    Praktikum 10B - Menambahkan Data pada Database

--- App/Model/Pustaka/Author.php
+++ App/Model/Pustaka/Author.php
@@ -1,26 +1,33 @@
 <?php
 
 namespace App\Model\Pustaka;
-class Author
-{
-    public $name;
-    public $description;
 
-    public function __construct($name, $description) {
-        $this->name = $name;
-        $this->description = $description;
+use App\Model\Model;
+
+class Author extends Model
+{
+    public int $id;
+    public string $name;
+    public string $description;
+
+    public function save()
+    {
+        try {
+            $stmt = $this->db->prepare("INSERT INTO author (id, name , description) VALUES (:id, :name, :description)");
+            $stmt->bindParam(':id', $this->id);
+            $stmt->bindParam(':name', $this->name);
+            $stmt->bindParam(':description', $this->description);
+            $result = $stmt->execute();
+        } catch (\PDOException $e) {
+            http_response_code(500);
+            $result = ["message" => $e->getMessage()];
+        }
+        return $result;
     }
 
-    public function show($type): array {
-        if ($type === 'name') {
-            return ['name' => $this->name];
-        } elseif ($type === 'description') {
-            return ['description' => $this->description];
-        } else {
-            return [
-                'name' => $this->name,
-                'description' => $this->description
-            ];
-        }
+
+    public function show(): array
+    {
+        return [];
     }
 }
